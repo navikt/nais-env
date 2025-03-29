c0749ca9c94af231055a9e037ea78df5c8d30bcf
Bjørn Gilstad <bjorn.gilstad@nav.no>
2025-03-29 12:43:44 +0100

    Add option to print and save to file

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,46 @@
-use std::{env, process::Command};
+use std::{collections::HashMap, env, process::Command};
+
+use clap::Parser;
 mod kubernetes_client;
 mod nais;
 
+/// Set up configuration from Nais locally
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Save Nais configuration to file
+    #[arg(short, long)]
+    file: Option<String>,
+
+    /// Path to nais.yaml
+    #[arg(short, long)]
+    config: String,
+
+    /// Print secrets
+    #[arg(short, long)]
+    print: bool,
+
+    /// Spawn shell with secrets as enviroment variables
+    #[arg(short, long)]
+    shell: bool,
+}
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    let args: Vec<String> = std::env::args().collect();
-    let nais_config_path = if args.len() > 1 {
-        &args[1]
-    } else {
-        panic!("Missing config file")
-    };
+    let args = Args::parse();
 
-    let nais_config = nais::NaisConfigLoader::new(nais_config_path.clone()).expect("data");
+    let file_path = args.file;
+    let config_file = args.config;
+
+    let nais_config = nais::NaisConfigLoader::new(config_file.clone()).expect("data");
 
     let current_namespace = nais_config.get_namespace();
-    let current_deployment = nais_config.get_deployment();
 
-    let kubernetes_client = kubernetes_client::KubernetesClient::new(
-        current_namespace.to_string(),
-        current_deployment.to_string(),
-    )
-    .await
-    .expect("Failed to create Kubernetes client");
+    let kubernetes_client = kubernetes_client::KubernetesClient::new(current_namespace.to_string())
+        .await
+        .expect("Failed to create Kubernetes client");
 
     let mut env_vars = nais_config.get_env_vars();
-
-    match kubernetes_client
-        .get_deployment(nais_config.get_deployment())
-        .await
-    {
-        Ok(_) => println!("Successfully fetched deployment"),
-        Err(e) => panic!("Failed to fetch deployment: {}", e),
-    }
 
     let secret_keys = nais_config.get_env_var_from_secret_keys();
 
@@ -47,6 +56,50 @@ async fn main() -> std::io::Result<()> {
         env_vars.insert(key.clone(), value.clone());
     }
 
+    if let Some(file) = file_path {
+        match save_env_vars_to_file(&file, &env_vars) {
+            Ok(_) => println!("Successfully saved environment variables to file: {}", file),
+            Err(e) => eprintln!("Failed to save environment variables to file: {}", e),
+        }
+    }
+
+    if args.print {
+        println!("Environment Variables:");
+        for (key, value) in &env_vars {
+            println!("{}={}", key, value);
+        }
+    }
+
+    if args.shell {
+        println!("Spawning shell with environment variables...");
+        spawn_interactive_shell(&env_vars)?;
+    }
+
+
+    Ok(())
+}
+
+fn save_env_vars_to_file(
+    filename: &str,
+    env_vars: &HashMap<String, String>,
+) -> std::io::Result<()> {
+    // Convert relative path to absolute path
+    let path = std::path::Path::new(filename);
+
+    // Create file for writing
+    let mut file = std::fs::File::create(path)?;
+
+    // Write each environment variable as KEY=VALUE
+    for (key, value) in env_vars {
+        use std::io::Write;
+        writeln!(file, "{}={}", key, value)?;
+    }
+
+    println!("Environment variables saved to: {:?}", path);
+    Ok(())
+}
+
+fn spawn_interactive_shell(env_vars: &HashMap<String, String>) -> std::io::Result<()> {
     let shell = if cfg!(target_os = "windows") {
         String::from("cmd")
     } else {
@@ -55,7 +108,7 @@ async fn main() -> std::io::Result<()> {
 
     // Create command for the shell
     let mut command = Command::new(shell);
-    for (key, value) in &env_vars {
+    for (key, value) in env_vars {
         command.env(key, value);
     }
 
